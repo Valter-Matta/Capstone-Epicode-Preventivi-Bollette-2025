b5a9072ca7f362063641a6ef93401685dd4d41ce
Valter Matta <mattavalter98@gmail.com>
2025-03-04 10:39:16 +0100

    push

---
+++ Preventivi-Bollette-BE/src/test/java/it/epicode/preventivi/PreventiviApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.preventivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
